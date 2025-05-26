10b0da1d8ae0cd1adf8abe9ce7f148c35c987053
EmmittMendez <77180330+EmmittMendez@users.noreply.github.com>
2025-05-26 03:19:16 -0600

    Product

--- src/utils/authApi.js
+++ src/utils/authApi.js
@@ -1,7 +1,8 @@
 // utils/authApi.js
+import { apiUrl } from "./api";
 
 export async function registerUser(data) {
-  const res = await fetch("http://localhost:3001/api/register", {
+  const res = await fetch(`${apiUrl}/api/register`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -13,7 +14,7 @@ export async function registerUser(data) {
 }
 
 export async function loginUser(data) {
-  const res = await fetch("http://localhost:3001/api/login", {
+  const res = await fetch(`${apiUrl}/api/login`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
